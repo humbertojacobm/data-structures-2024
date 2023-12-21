e7e878cf74118538e4dcb11384a5f25fd4d0cb0e
Humberto Jacob Mori Guerra <humberto.mori@encora.com>
2023-12-21 15:25:07 -0500

    practing while with arrays

--- 01 arrays/01 easy/01exercise.js
+++ 01 arrays/01 easy/01exercise.js
@@ -1,25 +1,59 @@
 /**
- * @param {string[][]} paths
- * @return {string}
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
  */
-var destCity = function(paths) {    
-    let stepPathEnd = "";
-    for (let index = 0; index < paths.length; index++) {
-        const currentPath = paths[index];
-        const pathStart = currentPath[0];
-        const pathEnd = currentPath[1];        
-        stepPathEnd = pathEnd;
 
-        if(index == 0){
-            continue;
-        }
-    
-        if(pathEnd == paths[index-1][1])
-        {
-            stepPathEnd = pathEnd2;
-        }            
+var twoSum = function (nums, target) {
+    let result = "no response";
+    if (!nums || nums.length < 2) {
+        result = "the length of nums should be 2 or greater"
+        return result;
     }
-    return stepPathEnd.toString();
+
+    let filterNumbers = [];
+    for (let index = 0; index < nums.length; index++) {
+        let currentValue = nums[index];
+        if (currentValue < target) {
+            filterNumbers.push(index);
+        }        
+    }
+
+    if (filterNumbers.length < 2) {
+        result = "the nums is wrong, because it should have at least 2 to sum the target"
+        return result;
+    }
+
+    if (filterNumbers.length == 2 && nums[filterNumbers[0]]+nums[filterNumbers[1]] === target) {
+        return filterNumbers;
+    }
+
+    let i = 0; 
+    let targetSum = 0;
+    let finalIndexes = [];
+    while (targetSum !== target) {
+        targetSum += nums[i];
+        if (targetSum<=target) {
+            console.log("index less than target", i);
+            finalIndexes.push(i);               
+        }else{
+            console.log("enter here", i);
+            targetSum -= nums[i];                        
+        }
+        i++;   
+        if (i >= nums.length && targetSum !== target) {
+            finalIndexes.shift();
+            i = finalIndexes[0];
+            finalIndexes.length = 0;
+            targetSum = 0;
+        }      
+    }
+
+    result = finalIndexes;
+    return result;    
 };
 
-console.log(destCity([["a","b"],["b","c"], ["c","d"]]));
+// console.log(twoSum([2], 9));
+// console.log(twoSum([2,3,4,11,15], 9));
+// console.log(twoSum([2,7,11,15], 9));
+console.log(twoSum([3,2,4], 6));
